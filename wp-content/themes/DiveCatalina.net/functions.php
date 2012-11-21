95286f79360178debdbb776a969bdaf98ea4cbf5
Jem <jemsomnia@gmail.com>
2012-11-21 03:20:09 -0800

    added color picker panel but I can't get it to work :(

--- wp-content/themes/DiveCatalina.net/functions.php
+++ wp-content/themes/DiveCatalina.net/functions.php
@@ -4,6 +4,14 @@
  *  as well as utility functions that we can use
  */
 
+//define your color pickers
+$colorPickers = array(
+array('ID' => 'background', 'label' => 'Background', 'default_color' => '#fff000'),
+array('ID' => 'textcolor', 'label' => 'Text Color', 'default_color' => '#222222'),
+array('ID' => 'color_3', 'label' => 'Color 3', 'default_color' => '#002c84'),
+array('ID' => 'color_4', 'label' => 'Color 4', 'default_color' => '#002c84'),
+);
+
 // register 3 widget areas
 function theme_widgets_init() {
     // Area 1 = Primary
@@ -116,3 +124,78 @@ class SimpleNavWalker extends Walker_Nav_Menu {
 			$item_output, $item);
 	}
 }
+
+// add change color functionality
+add_action('admin_menu', 'themeoptions_admin_menu'); //action to display the menu
+function themeoptions_admin_menu() 
+{
+	add_theme_page('color picker', 'Color Picker', 'read', 'color_picker_option', 'color_picker_option_page');
+}
+
+add_action('init', 'load_theme_scripts');
+function load_theme_scripts() {
+	wp_enqueue_style( 'farbtastic' );
+	wp_enqueue_script( 'farbtastic' );
+}
+
+function color_picker_option_page() 
+{
+	global $colorPickers;
+
+	/*The admin page*/
+	if ( isset($_POST['update_options'])) { color_picker_option_update(); }
+	?>
+	<div class="wrap">
+		<div id="icon-themes" class="icon32"><br /></div>
+		<h2>Color Picker</h2><br/>
+		<form method="POST" action="">	
+			<table class="widefat color_picker_options">
+			<thead><tr><th colspan="2">Customize the colors of the website</th></tr></thead>
+			<tbody>
+			<?php
+			foreach($colorPickers as $colorPicker):
+				$colorOption = get_option($colorPicker['ID']);
+				if (empty($colorOption) || $colorOption == ''):
+					add_option($colorPicker['ID'], $colorPicker['default_color']);
+				endif;
+			?>
+			<tr valign="top">
+				<th width="200px" scope="row"><label for="<?php echo $colorPicker['ID']; ?>"><?php _e($colorPicker['label']); ?></label></th>
+				<td>
+					<input type="text" class="color-picker" id="<?php echo $colorPicker['ID']; ?>" value="<?php echo get_option($colorPicker['ID']); ?>" name="<?php echo $colorPicker['ID']; ?>" />
+					<div id="<?php echo $colorPicker['ID']; ?>_color"></div>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+			</tbody>
+			<tfoot><tr><th>&nbsp;</th><th>&nbsp;</th></tr></tfoot>
+			</table>
+			<p><input type="submit" name="update_options" value="Update Options" class="button-primary" /></p>
+		</form>
+	</div>
+	<script type="text/javascript">
+		jQuery(document).ready(function($){
+			var colorPickers = $('.color-picker');
+			console.log(colorPickers);
+			for (e in colorPickers) {
+				if (colorPickers[e].id != undefined ) {
+					var colorPickerID = colorPickers[e].id;
+					$('#'+ colorPickerID + '_color').farbtastic('#' + colorPickerID);
+				}
+			}		
+		});
+	</script>
+
+<?php 
+}
+
+
+function color_picker_option_update()
+{
+	global $colorPickers;
+	foreach($colorPickers as $colorPicker) {
+		update_option($colorPicker['ID'], esc_html($_POST[$colorPicker['ID']]));
+	}
+}
+
+?>
