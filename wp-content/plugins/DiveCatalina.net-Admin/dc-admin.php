4ae2d8c3af79f2f2f7de39785beab69e1c561bf6
Jem <jemsomnia@gmail.com>
2012-11-20 23:19:29 -0800

    adding change color functionality

--- wp-content/plugins/DiveCatalina.net-Admin/dc-admin.php
+++ wp-content/plugins/DiveCatalina.net-Admin/dc-admin.php
@@ -89,4 +89,147 @@ function media_custom_columns($column_name, $id) {
 	}
 }
 
+
+add_action('admin_menu', 'plugin_admin_add_page');
+function plugin_admin_add_page() {
+	add_theme_page('Change Color', 'Colors', 'manage_options', 'plugin', 'color_options');
+}
+
+function color_options() {
+?>
+	<div>
+	<h2>Change Colors</h2>
+	Options relating to changing colors.
+	</div>	
+	<?php 
+}
+
+/*
+// JEM TESTING STUFF
+if (!class_exists("ChangeColors")) {
+	class ChangeColors {
+		var $adminOptionsName = "ChangeColorsAdminOptions";
+		function ChangeColors() { //constructor
+			
+		}
+		function init() {
+			$this->getAdminOptions();
+		}
+		//Returns an array of admin options
+		function getAdminOptions() {
+			$changeColorsAdminOptions = array('show_header' => 'true',
+				'add_content' => 'true', 
+				'comment_author' => 'true', 
+				'content' => '');
+			$devOptions = get_option($this->adminOptionsName);
+			if (!empty($devOptions)) {
+				foreach ($devOptions as $key => $option)
+					$changeColorsAdminOptions[$key] = $option;
+			}				
+			update_option($this->adminOptionsName, $changeColorsAdminOptions);
+			return $changeColorsAdminOptions;
+		}
+		
+		function addHeaderCode() {
+			$devOptions = $this->getAdminOptions();
+			if ($devOptions['show_header'] == "false") { return; }
+			?>
+<!-- ChangeColors Was Here -->
+			<?php
+		
+		}
+		function addContent($content = '') {
+			$devOptions = $this->getAdminOptions();
+			if ($devOptions['add_content'] == "true") {
+				$content .= $devOptions['content'];
+			}
+			return $content;
+		}
+		function authorUpperCase($author = '') {
+			$devOptions = $this->getAdminOptions();
+			if ($devOptions['comment_author'] == "true") {
+				$author = strtoupper($author);
+			}
+			return $author;
+		}
+		//Prints out the admin page
+		function printAdminPage() {
+					$devOptions = $this->getAdminOptions();
+										
+					if (isset($_POST['update_ChangeColorsSettings'])) { 
+						if (isset($_POST['devloungeHeader'])) {
+							$devOptions['show_header'] = $_POST['devloungeHeader'];
+						}	
+						if (isset($_POST['devloungeAddContent'])) {
+							$devOptions['add_content'] = $_POST['devloungeAddContent'];
+						}	
+						if (isset($_POST['devloungeAuthor'])) {
+							$devOptions['comment_author'] = $_POST['devloungeAuthor'];
+						}	
+						if (isset($_POST['devloungeContent'])) {
+							$devOptions['content'] = apply_filters('content_save_pre', $_POST['devloungeContent']);
+						}
+						update_option($this->adminOptionsName, $devOptions);
+						
+						?>
+<div class="updated"><p><strong><?php _e("Settings Updated.", "ChangeColors");?></strong></p></div>
+					<?php
+					} ?>
+<div class=wrap>
+<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+<h2>Change Colors</h2>
+<h3>Content to Add to the End of a Post</h3>
+<textarea name="devloungeContent" style="width: 80%; height: 100px;"><?php _e(apply_filters('format_to_edit',$devOptions['content']), 'ChangeColors') ?></textarea>
+<h3>Allow Comment Code in the Header?</h3>
+<p>Selecting "No" will disable the comment code inserted in the header.</p>
+<p><label for="devloungeHeader_yes"><input type="radio" id="devloungeHeader_yes" name="devloungeHeader" value="true" <?php if ($devOptions['show_header'] == "true") { _e('checked="checked"', "ChangeColors"); }?> /> Yes</label>&nbsp;&nbsp;&nbsp;&nbsp;<label for="devloungeHeader_no"><input type="radio" id="devloungeHeader_no" name="devloungeHeader" value="false" <?php if ($devOptions['show_header'] == "false") { _e('checked="checked"', "ChangeColors"); }?>/> No</label></p>
+
+<h3>Allow Content Added to the End of a Post?</h3>
+<p>Selecting "No" will disable the content from being added into the end of a post.</p>
+<p><label for="devloungeAddContent_yes"><input type="radio" id="devloungeAddContent_yes" name="devloungeAddContent" value="true" <?php if ($devOptions['add_content'] == "true") { _e('checked="checked"', "ChangeColors"); }?> /> Yes</label>&nbsp;&nbsp;&nbsp;&nbsp;<label for="devloungeAddContent_no"><input type="radio" id="devloungeAddContent_no" name="devloungeAddContent" value="false" <?php if ($devOptions['add_content'] == "false") { _e('checked="checked"', "ChangeColors"); }?>/> No</label></p>
+
+<h3>Allow Comment Authors to be Uppercase?</h3>
+<p>Selecting "No" will leave the comment authors alone.</p>
+<p><label for="devloungeAuthor_yes"><input type="radio" id="devloungeAuthor_yes" name="devloungeAuthor" value="true" <?php if ($devOptions['comment_author'] == "true") { _e('checked="checked"', "ChangeColors"); }?> /> Yes</label>&nbsp;&nbsp;&nbsp;&nbsp;<label for="devloungeAuthor_no"><input type="radio" id="devloungeAuthor_no" name="devloungeAuthor" value="false" <?php if ($devOptions['comment_author'] == "false") { _e('checked="checked"', "ChangeColors"); }?>/> No</label></p>
+
+<div class="submit">
+<input type="submit" name="update_ChangeColorsSettings" value="<?php _e('Update Settings', 'ChangeColors') ?>" /></div>
+</form>
+ </div>
+					<?php
+				}//End function printAdminPage()
+	
+	}
+
+} //End Class ChangeColors
+
+if (class_exists("ChangeColors")) {
+	$dl_pluginSeries = new ChangeColors();
+}
+
+//Initialize the admin panel
+if (!function_exists("ChangeColors_ap")) {
+	function ChangeColors_ap() {
+		global $dl_pluginSeries;
+		if (!isset($dl_pluginSeries)) {
+			return;
+		}
+		if (function_exists('add_theme_page')) {
+	add_theme_page('Change Colors', 'Change Colors', 9, basename(__FILE__), array(&$dl_pluginSeries, 'printAdminPage'));
+		}
+	}	
+}
+
+//Actions and Filters	
+if (isset($dl_pluginSeries)) {
+	//Actions
+	add_action('admin_menu', 'ChangeColors_ap');
+	add_action('wp_head', array(&$dl_pluginSeries, 'addHeaderCode'), 1);
+	add_action('activate_devlounge-plugin-series/devlounge-plugin-series.php',  array(&$dl_pluginSeries, 'init'));
+	//Filters
+	add_filter('the_content', array(&$dl_pluginSeries, 'addContent'),1); 
+	add_filter('get_comment_author', array(&$dl_pluginSeries, 'authorUpperCase'));
+}
+*/
+
 ?>
