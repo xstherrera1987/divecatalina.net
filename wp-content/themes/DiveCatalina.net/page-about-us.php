8326274cbe227602d8d52e5445ec4add2df0beac
Joe Herrera <xstherrera1987@gmail.com>
2012-12-16 13:31:53 -0800

    fix meet the staff pages (still need styling)

--- wp-content/themes/DiveCatalina.net/page-about-us.php
+++ wp-content/themes/DiveCatalina.net/page-about-us.php
@@ -12,16 +12,53 @@ get_header(); ?>
 				// About us Content
 				the_post();
 				the_content();
+				echo '<br/><hr/><br/>';
 				
 				// get Meet the Staff from DB
 				$subpages = get_pages( array( 'child_of' => $post->ID, 'parent' => $post->ID, hierarchical => '1', 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
 				
-				$content = $subpages[0]->post_content;
-				$content = apply_filters( 'the_content', $content );
+				$meetstaff = $subpages[0];
+				$meetstaff_content = apply_filters( 'the_content', $meetstaff->post_content );
 				
-				// Meet the Staff content
-				echo '<br/><hr/><br/>';
-				echo '<div class="subpage">'.$content.'</div>';
+				// Meet the Staff content (title and maybe short description)
+				echo '<div class="subpage">';
+				echo $meetstaff_content;
+				echo '<br />';
+				
+				// get each staff page
+				$staffpages = get_pages( array( 'child_of' => $meetstaff->ID, 'parent' => $meetstaff->ID, hierarchical => '1', 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
+				
+				echo '	<div id="staffpages">';
+				foreach($staffpages as $pg) {
+				  
+				  echo '<div class="staff">';
+				  // picture first (so its on left)
+				  echo '  <div class="portrait">';
+				  
+				  $args = array('post_type' => 'attachment', 'post_mime_type' => 'image', 'post_parent' => $pg->ID);
+				  $images = get_posts( $args );
+				  // only use first image
+				  $img = $images[0];
+				  $img_meta = wp_get_attachment_metadata($img->ID);
+				  $width = $img_meta['width'];
+				  $height = $img_meta['height'];
+				  $img_data = wp_get_attachment_image_src($img->ID, array($width,$height));
+				  $url = $img_data[0];
+				  echo '<img src="'.$url.'" height="'.$height.'" width="'.$width.'" />';
+				  
+				  echo '  </div>';
+				  
+				  // get content
+				  echo '  <div class="staffbio">';
+				  $staff = $pg->post_content;
+				  echo apply_filters( 'the_content', $staff );
+				  echo '  </div>';
+				  
+				  echo '</div>';
+				}
+				echo '	</div>';
+				
+				echo '</div>';
 			?>
     	</div>
 	</div>
---
+++ wp-content/themes/DiveCatalina.net/page-meet-the-staff.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Template Name:  Redirect -> Meet the Staff
+ * Description: redirects to About Us, Staff section
+ */ 
+// get parent page (about us)
+$aboutus_url = get_permalink($post->post_parent);
+// anchor into the staff section
+$aboutus_url .= "#staffpages";
+// go there
+header( 'Location: '.$aboutus_url ) ;
