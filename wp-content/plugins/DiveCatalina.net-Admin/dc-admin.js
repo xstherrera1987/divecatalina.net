c29039eff2af14acf3ee40cea7d9d338305c8a67
Joe Herrera <xstherrera1987@gmail.com>
2012-12-16 03:27:08 -0800

    fix main wp-admin menu

--- wp-content/plugins/DiveCatalina.net-Admin/dc-admin.js
+++ wp-content/plugins/DiveCatalina.net-Admin/dc-admin.js
@@ -1,4 +1,7 @@
 jQuery(document).ready( function() {
-	// rename "Documentation to WP Documentation"	
-	jQuery("#wp-admin-bar-documentation").children().html("WP Documentation");
+	jQuery("#wp-admin-bar-about > a").attr("href", "http://codex.wordpress.org/");	
+	jQuery("#wp-admin-bar-documentation > a").html("DiveCatalina.net Documentation");
+	jQuery("#wp-admin-bar-documentation > a").attr("href", "http://divecatalina.net/documentation");
+	jQuery("#wp-admin-bar-support-forums").hide();
+	jQuery("#wp-admin-bar-feedback").hide();
 });
