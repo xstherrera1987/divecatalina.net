ecf3263b57471cdcd11facba4e1c1389c59f9ab4
Joe Herrera <xstherrera1987@gmail.com>
2013-01-25 23:21:48 -0800

    meet the staff pages sort by page's order attribute (lower means higher ordering)

--- wp-content/themes/DiveCatalina.net/page-about-us.php
+++ wp-content/themes/DiveCatalina.net/page-about-us.php
@@ -1,78 +1,89 @@
-<?php 
+<?php
 /**
  * Template Name:  About Us Template
  * Description: A Page Template that displays the about and staff page
- */ 
+ */
+get_header();
+?>
 
-get_header(); ?>
+<div id="content">
+  <div id="main-content">
+    <?php
+    // About us Content
+    the_post();
+    echo '<h1>' . get_the_title($post->ID) . '</h1>';
+    echo '<div class="clear"></div>';
 
-	<div id="content">
-    	<div id="main-content">
-			<?php
-				// About us Content
-				the_post();
-				echo '<h1>'.get_the_title($post->ID).'</h1>';
-				echo '<div class="clear"></div>';
-				
-				$aboutusimgargs = array('post_type' => 'attachment', 'post_mime_type' => 'image', 'post_parent' => $post->ID);
-				$aboutusimages = get_posts( $aboutusimgargs );
-				// only use first image
-				$img = $aboutusimages[0];
-				$img_data = wp_get_attachment_image_src($img->ID, "full");
-				$url = $img_data[0];
-				echo '<img src="'.$url.'" height="211" width="281" class="aboutus-imgleft" />';
-				
-				the_content();
-				echo '<br/><hr/><br/>';
-				
-				// get Meet the Staff from DB
-				$subpages = get_pages( array( 'child_of' => $post->ID, 'parent' => $post->ID, hierarchical => '1', 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
-				
-				$meetstaff = $subpages[0];
-				$meetstaff_content = apply_filters( 'the_content', $meetstaff->post_content );
-				
-				// Meet the Staff content (title and maybe short description)
-				echo '<div id="meetthestaff">';
-				echo $meetstaff_content;
-				echo '<br />';
-				
-				// get each staff page
-				$staffpages = get_pages( array( 'child_of' => $meetstaff->ID, 'parent' => $meetstaff->ID, hierarchical => '1', 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
-				
-				echo '	<div id="staffpages">';
-				for($i=count($staffpages)-1; $i>=0; $i--) {
-				  $pg = $staffpages[$i];
-				  
-				  echo '  <div class="staff">';
-				  // picture first (so its on left)
-				  echo '  <div class="portrait">';
-				  
-				  $args = array('post_type' => 'attachment', 'post_mime_type' => 'image', 'post_parent' => $pg->ID);
-				  $images = get_posts( $args );
-				  // only use first image
-				  $img = $images[0];
-				  $img_data = wp_get_attachment_image_src($img->ID);
-				  $url = $img_data[0];
-				  echo '<img src="'.$url.'" height="150" width="120" class="portrait-img" />';
-				  
-				  echo '  </div>';
-				  
-				  // get content
-				  echo '  <div class="staffbio">';
-				  $staff = $pg->post_content;
-				  echo apply_filters( 'the_content', $staff );
-				  echo '  </div>';
-				  
-				  echo '</div>';
-				  echo '  <div class="clear"></div>';
-				  echo '<br />';
-				}
-				echo '	</div>';
-				
-				echo '</div>';
-			?>
-    	</div>
-	</div>
+    $aboutusimgargs = array('post_type' => 'attachment', 'post_mime_type' => 'image', 'post_parent' => $post->ID);
+    $aboutusimages = get_posts($aboutusimgargs);
+    // only use first image
+    $img = $aboutusimages[0];
+    $img_data = wp_get_attachment_image_src($img->ID, "full");
+    $url = $img_data[0];
+    echo '<img src="' . $url . '" height="211" width="281" class="aboutus-imgleft" />';
+
+    the_content();
+    echo '<br/><hr/><br/>';
+
+    // get Meet the Staff from DB
+    $subpages = get_pages(array('child_of' => $post->ID, 'parent' => $post->ID, hierarchical => '1', 'sort_column' => 'post_date', 'sort_order' => 'desc'));
+
+    $meetstaff = $subpages[0];
+    $meetstaff_content = apply_filters('the_content', $meetstaff->post_content);
+
+    // Meet the Staff content (title and maybe short description)
+    echo '<div id="meetthestaff">';
+    echo $meetstaff_content;
+    echo '<br />';
+
+    // get each staff page
+    $staffpages = get_pages(array('child_of' => $meetstaff->ID, 'parent' => $meetstaff->ID, hierarchical => '1', 'sort_column' => 'post_date', 'sort_order' => 'desc'));
+    
+    // compare pages by 
+    function pageComparator($a, $b) {
+      $a_order = get_post_field('menu_order', $a->ID);
+      $b_order = get_post_field('menu_order', $b->ID);
+      
+      if ($a_order == $b_order) return 0;
+      return $a_order < $b_order ? 1 : -1;
+    }
+    
+    usort( $staffpages, pageComparator);
+    
+    echo '	<div id="staffpages">';
+    for ($i = count($staffpages) - 1; $i >= 0; $i--) {
+      $pg = $staffpages[$i];
+
+      echo '  <div class="staff">';
+      // picture first (so its on left)
+      echo '  <div class="portrait">';
+
+      $args = array('post_type' => 'attachment', 'post_mime_type' => 'image', 'post_parent' => $pg->ID);
+      $images = get_posts($args);
+      // only use first image
+      $img = $images[0];
+      $img_data = wp_get_attachment_image_src($img->ID);
+      $url = $img_data[0];
+      echo '<img src="' . $url . '" height="150" width="120" class="portrait-img" />';
+
+      echo '  </div>';
+
+      // get content
+      echo '  <div class="staffbio">';
+      $staff = $pg->post_content;
+      echo apply_filters('the_content', $staff);
+      echo '  </div>';
+
+      echo '</div>';
+      echo '  <div class="clear"></div>';
+      echo '<br />';
+    }
+    echo '	</div>';
+
+    echo '</div>';
+    ?>
+  </div>
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
